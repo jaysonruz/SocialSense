145b71eb10c17580945252838f2d2869dffd029b
jaysonruz <jaysonruzario@gmail.com>
2023-07-30 21:16:31 +0530

    create routes for login + fetch ig posts

--- services/apify_instagram.py
+++ services/apify_instagram.py
@@ -6,11 +6,12 @@ import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-def scrape_instagram_data(instagram_id,all_posts=False):
-    """
-    instagram_id: str ( id of instagram )
-    all_posts: bool  ( default: False )
-    """
+# services/apify_instagram.py
+import httpx
+import os
+from typing import List
+
+def scrape_instagram_data(instagram_id: str, all_posts: bool = False) -> List[dict]:
     APIFY_API_TOKEN = os.environ.get("APIFY_API_TOKEN")
     if not APIFY_API_TOKEN:
         raise ValueError("APIFY_API_TOKEN environment variable not found. Please set it in your .env file.")
@@ -28,47 +29,41 @@ def scrape_instagram_data(instagram_id,all_posts=False):
     }
     
     run_input_fast = {
-    "directUrls": [f"https://www.instagram.com/{instagram_id}/"],
-    "resultsLimit": 1,
-    "resultsType": "details",
-    "searchLimit": 1,
-    "searchType": "hashtag",
-    "extendOutputFunction": "",
-    "extendScraperFunction": "",
-    "customData": {}
+        "directUrls": [f"https://www.instagram.com/{instagram_id}/"],
+        "resultsLimit": 1,
+        "resultsType": "details",
+        "searchLimit": 1,
+        "searchType": "hashtag",
+        "extendOutputFunction": "",
+        "extendScraperFunction": "",
+        "customData": {}
     }
 
     if all_posts:
         print("scraping with slow method !")
         run_input = run_input_slow
-        
-        # Run the actor and wait for it to finish
-        run = client.actor("apify/instagram-scraper").call(run_input=run_input)
+    else:
+        print(f"scraping with fast method ! for ({instagram_id})")
+        run_input = run_input_fast
 
-        # Fetch and collect actor results from the run's dataset (if there are any)
-        results = []
-        for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-            results.append(item)
+    # Run the actor and wait for it to finish
+    run = client.actor("apify/instagram-scraper").call(run_input=run_input)
+
+    # Fetch and collect actor results from the run's dataset (if there are any)
+    results = []
+    for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+        results.append(item)
         
+    if all_posts:
         return results
     else:
-        print("scraping with fast method !")
-        run_input = run_input_fast
-        
-        # Run the actor and wait for it to finish
-        run = client.actor("apify/instagram-scraper").call(run_input=run_input)
+        return results[0]["latestPosts"] if len(results) >= 1 else []
 
-        # Fetch and collect actor results from the run's dataset (if there are any)
-        results = []
-        for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-            results.append(item)
-        
-        return results[0]["latestPosts"]
 
 if __name__ == "__main__":
-    instagram_id = "nike"  # Replace with the desired Instagram profile ID
+    instagram_id = "21n.78e"  # Replace with the desired Instagram profile ID
     scraped_data = scrape_instagram_data(instagram_id,all_posts=False)
-
+    print(scraped_data)
     # Save the result to a JSON file
     output_file = f"{instagram_id}_scraped_data.json"
     with open(output_file, "w") as f:
