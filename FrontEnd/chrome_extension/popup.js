a7e7ca859263f1f5819810d85e6cef8a7e322a89
Jayson Ruzario <jaysonruzario@gmail.com>
2023-08-11 14:48:33 +0530

    added highlighting to errors

--- FrontEnd/chrome_extension/popup.js
+++ FrontEnd/chrome_extension/popup.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
   const instagramInput = document.getElementById('instagramInput');
   const submitBtn = document.getElementById('submitBtn');
   const instagramList = document.getElementById('instagramList');
   const backend_url = "http://192.168.2.172:80";
-  
+
   submitBtn.addEventListener('click', async function () {
     const inputValue = instagramInput.value;
-  
+
     const response = await fetch(backend_url + '/instagram_posts', {
       method: 'POST',
       headers: {
@@ -16,37 +16,35 @@ document.addEventListener('DOMContentLoaded', function () {
         instagram_id: inputValue,
       }),
     });
-  
+
     if (response.ok) {
       const igPosts = await response.json();
-  
+
       // Clear the existing list items
       instagramList.innerHTML = '';
-  
+
       // Create and append new list items for each post in the response
       igPosts.forEach((post) => {
         const listItem = document.createElement('li');
-  
+
         const errorFreePostHtml = `<li class="no-error">
         <div class="row">
           <div class="col-4">
             <div class="ratio ratio-1x1"> <img id="post_image" src="${post.displayUrl_hosted}" class="img-fluid post-img"> </div>
           </div>
           <div class="col-8">
-            <p><strong>Caption:</strong>${post.caption}</p>
+            <p><strong>Caption:</strong><span class="caption">${post.caption}</span></p>
             <div class="row">
-      <div class="col-6">
-        <p>Total errors: ${post.total_errors}</p>
-      </div>
-      
-     </div>
-          
+              <div class="col-6">
+                <p>Total errors: ${post.total_errors}</p>
+              </div>
+            </div>
           </div>
         </div>
         <div class="post-container">
           <div style="flex: 1;"> </div>
         </div>
-      </li>`
+      </li>`;
         const errorPostHTML = `
         <li class="red">
             <div class="row">
@@ -59,14 +57,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="ratio ratio-1x1"> <img id="post_image" src="${post.displayUrl_hosted}" class="img-fluid post-img"> </div>
               </div>
               <div class="col-8">
-                <p><strong>Caption:</strong>${post.caption}</p>
+                <p><strong>Caption:</strong><span class="caption">${highlightCorrectionsInCap(post.caption, post.corrections_list)}</span></p>
                 <div class="row">
 					<div class="col-6">
 						<p>Total errors: ${post.total_errors}</p>
 					</div>
 					<div class="col-6 text-end">
 						<button class="btn btn-primary btn-sm btn1 light-grren fw-bold">Fix errors</button>
-						
 					</div>
 				 </div>
               </div>
@@ -74,27 +71,24 @@ document.addEventListener('DOMContentLoaded', function () {
 					<div class="row">
 						<div class="col-12">
 							<div class="fix-error-box position-relative" style="display: none;">
-				  	<p class="fw-bold">Total errors fixed: 3</p>
-					<p id="p1">${post.correction_results}</p>
+				  	<p class="fw-bold">Total errors fixed: ${post.total_errors}</p>
+					<p id="p1">${highlightCorrectionsInRes(post.correction_results, post.corrections_list)}</p>
 				  <p class="copy-icon"><img src="images/copy-icon.svg" class="img-fluid"></p>
 				  <div class="d-flex align-items-center">
 				  	<p class="fw-bold me-2 mb-0">Is it helpful?</p>
 					  <button type="button" class="btn btn-primary btn-sm me-2 btn-green btn2">Yes</button>
 					  <button type="button" class="btn btn-primary btn-sm btn-red btn2">No</button>
 				  </div>
-				  
 			</div>
 						</div>
 					</div>
 				</div>
-              
             </div>
             <div class="post-container">
               <div style="flex: 1;"> </div>
             </div>
-          </li>
-        `;
-        
+          </li>`;
+
         // Conditionally set the PostHTML based on the value of post.any_corrections
         let postHTML;
         if (post.any_corrections === true) {
@@ -102,10 +96,10 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
           postHTML = errorFreePostHtml;
         }
-        
+
         listItem.innerHTML = postHTML;
         instagramList.appendChild(listItem);
-        
+
         // Add event listener for the "Fix errors" button
         const fixErrorsBtn = listItem.querySelector('.btn1');
         const fixErrorsBox = listItem.querySelector('.fix-error-box');
@@ -120,10 +114,28 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         // Add event listeners for other buttons as needed (e.g., .btn2)
-
       });
     } else {
       console.error('Error fetching Instagram posts:', response.status, response.statusText);
     }
   });
+
+  function highlightCorrectionsInCap(text, corrections_list) {
+    corrections_list.forEach((correction) => {
+      const originalText = correction.text;    // highlight words to be corrected 
+      const regex = new RegExp(`\\b${originalText}\\b`, 'gi');
+      console.log(`DEBUG: IN CAPTION: Original Text: ${originalText}`);
+      text = text.replace(regex, `<span class="highlight">${originalText}</span>`);
+    });
+    return text;
+  }
+  function highlightCorrectionsInRes(text, corrections_list) {
+    corrections_list.forEach((correction) => {
+      const originalText = correction.correct; // higlight corrected words 
+      const regex = new RegExp(`\\b${originalText}\\b`, 'gi');
+      console.log(`DEBUG: IN RESULT: Corrected Text: ${originalText}`);
+      text = text.replace(regex, `<span class="highlight">${originalText}</span>`);
+    });
+    return text;
+  }
 });
